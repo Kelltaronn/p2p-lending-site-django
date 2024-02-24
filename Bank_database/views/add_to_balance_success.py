f119d574e96d14e87f2705747770f87c3a0cb684
Kelltaronn <138993725+Kelltaronn@users.noreply.github.com>
2024-02-24 18:00:56 +0100

    Revert "Transaction inactivating cycle"

--- Bank_database/views/add_to_balance_success.py
+++ Bank_database/views/add_to_balance_success.py
@@ -6,16 +6,10 @@ def add_to_balance_success(request,id):
 
     #THe problematic  binarry:
     #We need an If
-    if actual_transaction.activated == True:
-        new_balance = actual_transaction.szamla_id.aktualis_osszeg + actual_transaction.osszeg
-        actual_transaction.szamla_id.aktualis_osszeg = new_balance
+    new_balance = actual_transaction.szamla_id.aktualis_osszeg + actual_transaction.osszeg
+    actual_transaction.szamla_id.aktualis_osszeg = new_balance
 
-         #szamla id objektum:
-        actual_transaction.activated = False
-        actual_transaction.save()
-        actual_transaction.szamla_id.save()
-        #Transaction change to active to inactive:
-        return render(request,'add_to_balance_success.html',context)
-    else:
-        context = {}
-        return render(request,'add_to_balance_unsuccess.html',context)
+    actual_transaction.szamla_id.save() #szamla id objektum:
+    #Transaction change to active to inactive:
+    context = {}
+    return render(request,'add_to_balance_success.html',context)
